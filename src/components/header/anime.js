ec22f3b936fc93e0582fdeffbccc247196793fd4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-24 16:19:59 +0530

    created anime

---
+++ src/components/header/anime.js
@@ -0,0 +1,15 @@
+export const opacity = {
+    initial: {
+        opacity: 0,
+    },
+    open: {
+        opacity: 1,
+        transition: {
+            duration: 0.35
+        }
+    },
+    closed: {
+        opacity: 0,
+        transition: {duration: 0.35}
+    }
+}
