67f6078cc51f54b705ea2026d774a8324e062688
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-24 16:20:26 +0530

    created index

---
+++ src/components/hero/Index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from '../header/Index'
+
+function LandingPage() {
+  return (
+    <div data-scroll-section className='first w-full h-[100vh]'>
+        <div className='page1 relative'>
+            <div className=''>
+                <Header/>
+            </div>
+            {/* Hero section video */}
+            <div className='heroVideo'>
+                <video 
+                    className='w-full h-[100vh] object-cover'
+                    src="https://res.cloudinary.com/mkpill/video/upload/v1691860071/mp_intro_robot_v2_32611febe2.mp4"
+                    autoPlay
+                    loop
+                    muted
+                >
+                </video>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
