512f67556d7fb923c48290e09b31b08235714abe
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-24 16:49:44 +0530

    updated app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Cursor from '@/components/cursor/Index';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Cursor/>
+        {children}
+      </body>
     </html>
   );
 }
