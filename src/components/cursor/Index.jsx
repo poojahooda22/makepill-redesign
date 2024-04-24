201dab8bd9c267528f6dcd40dad91bb1bb79e70e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-24 16:50:01 +0530

    created cursor

---
+++ src/components/cursor/Index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './style.module.css'
+
+
+function Cursor() {
+  return (
+    <div className={``}>
+        <div className={`circle ${styles.circle}`}></div>
+        <div className={`circle ${styles.circle}`}></div>
+        <div className={`circle ${styles.circle}`}></div>
+        <div className={`circle ${styles.circle}`}></div>
+        <div className={`circle ${styles.circle}`}></div>
+        <div className={`circle ${styles.circle}`}></div>
+        <div className={`circle ${styles.circle}`}></div>
+        <div className={`circle ${styles.circle}`}></div>
+        <div className={`circle ${styles.circle}`}></div>
+        <div className={`circle ${styles.circle}`}></div>
+    </div>
+  )
+}
+
+export default Cursor
