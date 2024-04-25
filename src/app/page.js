81567783cd0b5b167d0927dc298ac6f2584dc057
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-25 17:43:36 +0530

    updated page

--- src/app/page.js
+++ src/app/page.js
@@ -12,30 +12,30 @@ export default function Home() {
   return (
 
     <LocomotiveScrollProvider
-  options={
-    {
-      smooth: true,
-      // ... all available Locomotive Scroll instance options 
-    }
-  }
-  watch={
-    [
-      //..all the dependencies you want to watch to update the scroll.
-      //  Basicaly, you would want to watch page/location changes
-      //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-    ]
-  }
-  containerRef={containerRef}
->
-  <main data-scroll-container ref={containerRef}>
-    <div className=" text-white bg-primary-main ">
-      {/* Your content here */}
-        
-       <Loader/>
-       <LandingPage/>
-    </div>
-  </main>
-</LocomotiveScrollProvider>
+      options={
+        {
+          smooth: true,
+          // ... all available Locomotive Scroll instance options 
+        }
+      }
+      watch={
+        [
+          //..all the dependencies you want to watch to update the scroll.
+          //  Basicaly, you would want to watch page/location changes
+          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+        ]
+      }
+      containerRef={containerRef}
+    >
+      <main data-scroll-container ref={containerRef}>
+        <div className=" text-white bg-primary-landing ">
+          {/* Your content here */}
+            
+          <Loader/>
+          <LandingPage/>
+        </div>
+      </main>
+    </LocomotiveScrollProvider>
     
   );
 }
