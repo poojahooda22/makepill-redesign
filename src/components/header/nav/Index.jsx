a3a66fb4ce321200e5b72389afa25b37186fa7b9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-25 21:53:23 +0530

    updated index

--- src/components/header/nav/Index.jsx
+++ src/components/header/nav/Index.jsx
@@ -71,7 +71,7 @@ function Nav() {
           <div className='overflow-hidden'>
             {
               ['Home', 'Works', 'Services', 'Contact', 'Vision', 'Home',
-              'Works', 'Services', 
+              'Works',  
               ].map((item, index) => 
               {
                 return (
