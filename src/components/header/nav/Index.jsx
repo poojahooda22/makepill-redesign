281065e0456d03d8d686e3de4069241de4f1f88a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-25 17:44:03 +0530

    updated index

--- src/components/header/nav/Index.jsx
+++ src/components/header/nav/Index.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { height } from '../anime';
+import styles from './Style.module.css'
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
+
 
 function Nav() {
+
+  useEffect(() => {
+    gsap.to(".page4 .underline", {
+      "--width": "100%",
+      scrollTrigger: {
+        trigger: ".page4 .underline",    
+        scrub: 2,
+      }
+    });
+  })
+
   return (
-    <div>
-      <div>
-        Navbar
+    <motion.div variants={height} initial="initial" animate="enter" exit="exit" 
+      className='page4 absolute top-0 left-0 w-full px-[2.5vw]  bg-red-300 text-white overflow-hidden'
+    >
+      <div className='wrapper flex items-center justify-between'>
+        <div className='m-0'>
+          <div></div>
+          {['Instagram', 'Twitter', 'Facebook', 'LinkedIn', 'Dribbble'].map((item,index) => {
+            return (
+              <div key={index} className={`ptag ${styles.ptag} `}>
+                <p className='leading-none ' >
+                  {item}
+                  <div className={`underline ${styles.underline}`}></div>
+                </p>              
+              </div>
+            )
+          })}
+        </div> 
+
+        <div>line</div>
+
+        <div className='content'>content</div>
+
+        <div className='pillImages'>pill</div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
