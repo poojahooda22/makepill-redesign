8252c8fedade619bfb4d9b12288addaf6722396f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-25 17:43:44 +0530

    updtaed header

--- src/components/header/anime.js
+++ src/components/header/anime.js
@@ -1,3 +1,5 @@
+const transition = {duration: 1, ease: [0.76, 0, 0.24, 1]}
+
 export const opacity = {
     initial: {
         opacity: 0,
@@ -11,5 +13,53 @@ export const opacity = {
     closed: {
         opacity: 0,
         transition: {duration: 0.35}
+    }
+}
+
+export const rotate = {
+    initial: {
+        rotate: 0
+    },
+    open: {
+        rotate: 6,
+        x: 3,
+        transition: {
+            duration: 0.35
+        }
+    },
+    closed: {
+        rotate: 0,
+        transition: {duration: 0.35}
+    }
+}
+
+export const rotate2 = {
+    initial: {
+        rotate: 0
+    },
+    open: {
+        rotate: -5,
+        x: -1, // move left half to the left
+        transition: {
+            duration: 0.35
+        }
+    },
+    closed: {
+        rotate: 0,
+        transition: {duration: 0.35}
+    }
+}
+
+export const height = {
+    initial: {
+        height: 0
+    },
+    enter: {
+        height: 'auto',
+        transition
+    },
+    exit: {
+        height: 0,
+        transition
     }
 }
