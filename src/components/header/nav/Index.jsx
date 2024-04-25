9690893ae7579c0b83f4a671359d0addb48f5607
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-25 22:01:32 +0530

    added

--- src/components/header/nav/Index.jsx
+++ src/components/header/nav/Index.jsx
@@ -20,7 +20,7 @@ function Nav() {
     },
     {
       title: 'Services',
-      link: 'https://res.cloudinary.com/mkpill/video/up43/expises_'
+      link: 'https://res.cloudinary.com/mkpill/videexpises_'
     },
     {
       title: 'Contact',
