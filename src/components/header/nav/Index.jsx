80257eda12d2de74bb4a1aa81bd0f126094bd445
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-25 22:01:27 +0530

    added

--- src/components/header/nav/Index.jsx
+++ src/components/header/nav/Index.jsx
@@ -24,7 +24,7 @@ function Nav() {
     },
     {
       title: 'Contact',
-      link: 'https://res.cloudinary.com//v44'
+      link: 'https://res.cloudinar/v44'
     },
     {
       title: 'Vision',
