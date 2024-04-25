e92e3f60b33f920092d53790338e442de6135d8f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-25 17:56:39 +0530

    updated index

--- src/components/header/nav/Index.jsx
+++ src/components/header/nav/Index.jsx
@@ -19,21 +19,26 @@ function Nav() {
 
   return (
     <motion.div variants={height} initial="initial" animate="enter" exit="exit" 
-      className='page4 absolute top-0 left-0 w-full px-[2.5vw]  bg-red-300 text-white overflow-hidden'
+      className='page4 absolute top-0 left-0 w-full h-full px-[2.5vw]  bg-red-300 text-white overflow-hidden'
     >
       <div className='wrapper flex items-center justify-between'>
-        <div className='m-0'>
+        <div className='m-0 flex flex-col items-start justify-between'>
           <div></div>
-          {['Instagram', 'Twitter', 'Facebook', 'LinkedIn', 'Dribbble'].map((item,index) => {
-            return (
-              <div key={index} className={`ptag ${styles.ptag} `}>
-                <p className='leading-none ' >
-                  {item}
-                  <div className={`underline ${styles.underline}`}></div>
-                </p>              
-              </div>
-            )
-          })}
+          <div>
+            {['Instagram', 'Twitter', 'Facebook', 'LinkedIn', 'Dribbble'].map((item,index) => {
+              return (
+                <div key={index} className={`ptag ${styles.ptag} `}>
+                  <p className='leading-none ' >
+                    {item}
+                    <div className={`underline ${styles.underline}`}></div>
+                  </p>              
+                </div>
+              )
+            })}
+          </div>
+          <div>
+            <p>hello@makepill.com</p>
+          </div>
         </div> 
 
         <div>line</div>
