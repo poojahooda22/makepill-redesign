3a5a0938758d9414dae120fbccad0bc29d560914
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-27 21:46:26 +0530

    added

--- src/components/header/nav/Index.jsx
+++ src/components/header/nav/Index.jsx
@@ -44,7 +44,7 @@ function Nav() {
   // useEffect(() => {
   //   gsap.to(".page4 .underline", {
   //     "--width": "100%",
-  //     scrollTrigger: {       
+  //     scrollTrigger      
   // })
 
   return (
