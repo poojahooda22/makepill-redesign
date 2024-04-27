e00fb898a8608b23be5a027b66563b69bea54fa4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-27 21:50:22 +0530

    added

--- src/components/header/nav/Index.jsx
+++ src/components/header/nav/Index.jsx
@@ -21,10 +21,10 @@ function Nav() {
       title: 'Services',
       link: 'https://res.cloudinary.com/mkpill/video/upload/v1689377144/expertises_x1_vp9_11ee1f3431.webm'
     },
-    {
-      title: 'Contact',
-      link: 'https://res.cloudinary.com/mkpill/video/upload/v1689377143/contact_x1_vp9_cc7a99e029.webm'
-    },
+    // {
+    //   title: 'Contact',
+    //   link: 'https://res.cloudinary.com/mkpill/video/upload/v1689377143/contact_x1_vp9_cc7a99e029.webm'
+    // },
     // {
     //   title: 'Vision',
     //   image: 'https://res.cloudinary.com/mkpill/video/upload/v1689377143/about_x1_vp9_af33c15d4f.webm'
