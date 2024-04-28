118ed9d0bbde1293080ed9619b17ac32b386c72d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-28 21:21:53 +0530

    created video

---
+++ src/components/video/Index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <div>
+      <div>
+        <video
+            
+        >
+
+        </video>
+      </div>
+    </div>
+  )
+}
+
+export default Index
