bcc7f2e6fc182be31759023450c5fcb39f2e947f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-28 19:12:55 +0530

    index updated

--- src/components/hero/Index.jsx
+++ src/components/hero/Index.jsx
@@ -9,15 +9,45 @@ function LandingPage() {
                 <Header/>
             </div>
             {/* Hero section video */}
-            <div className='heroVideo w-full -mt-[8vw]'>
-                <video 
+            <div className='heroVideo w-full flex items-center justify-center'>
+                <div className='-mt-[8vw]'>
+
+                </div>
+                {/* <video 
                     className='w-full h-[100vh] object-cover'
                     src="https://res.cloudinary.com/mkpill/video/upload/v1691860071/mp_intro_robot_v2_32611febe2.mp4"
                     autoPlay
                     loop
                     muted
                 >
-                </video>
+                </video> */}
+
+                <div className='w-full mt-[16vw] bg-red-400 z-[10] flex flex-col items-center justify-center'>
+                    <div className='flex'>
+                        <div>
+                            <h1>Digital</h1>
+                        </div>
+                        <div>
+                            <h1>products</h1>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>creatives</h1>
+                        </div>
+                        <div>
+                            <h1>& immersives<span>*</span></h1>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h4>We think and design unique experiences</h4>
+                        </div>
+                        <div>
+                            <h4>for tomorrownbsps innovative products</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
