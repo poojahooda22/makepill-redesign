9898a6d4553a66832a102dfbfabad9b762ff380a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-28 21:23:25 +0530

    updated index

--- src/components/video/Index.jsx
+++ src/components/video/Index.jsx
@@ -8,6 +8,7 @@ function Index() {
             autoPlay
             loop
             muted
+            className='w-full h-full object-cover'
             src="https://res.cloudinary.com/mkpill/video/upload/v1691655828/makepill_showreel_opti_a5c666148b.mp4"
         >
 
