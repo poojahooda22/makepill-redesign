83aea74df979002e156a04dadbb29460883f9dc3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-29 22:33:06 +0530

    updated

--- src/components/video/Index.jsx
+++ src/components/video/Index.jsx
@@ -35,7 +35,7 @@ function Video() {
           loop
           muted
           className='w-full h-full object-cover'
-            src="https://res.cloudinary.com/mkpill/video/upload/v1691655828/makepill_showreel_opti_a5c666148b.mp4"
+          src="https://res.cloudinary.com/mkpill/video/upload/v1691655828/makepill_showreel_opti_a5c666148b.mp4"
         >
         </video>
       </div>
