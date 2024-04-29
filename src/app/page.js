8f6f69e7f2831ddd0f844d38efca02d9f72ccf3f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-29 11:34:37 +0530

    updated page

--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,28 @@
 'use client'
 
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 import { useEffect, useRef } from 'react';
 import Loader from '@/components/loader/Index';
-import 'locomotive-scroll/dist/locomotive-scroll.css'
 import LandingPage from '@/components/hero/Index';
 import Video from '@/components/video/Index';
 import About from '@/components/services/Index';
 import Cursor from '@/components/cursor/Index';
 
+
+
 export default function Home() {
-  const containerRef = useRef(null)
+  
 
   return (
+    <main data-scroll-container ref={containerRef}>
+      <div className=" text-white bg-primary-main ">
+        {/* Your content here */}
+        <Cursor/> 
+        <Loader/>
+        <LandingPage/>
+        <Video/>
+        <About/>
+      </div>
+    </main>
 
-    <LocomotiveScrollProvider
-      options={
-        {
-          smooth: true,
-          // ... all available Locomotive Scroll instance options 
-        }
-      }
-      watch={
-        [
-          //..all the dependencies you want to watch to update the scroll.
-          //  Basicaly, you would want to watch page/location changes
-          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-        ]
-      }
-      containerRef={containerRef}
-    >
-      <main data-scroll-container ref={containerRef}>
-        <div className=" text-white bg-primary-main ">
-          {/* Your content here */}
-          <Cursor/> 
-          <Loader/>
-          <LandingPage/>
-          <Video/>
-          <About/>
-        </div>
-      </main>
-    </LocomotiveScrollProvider>
-    
   );
 }
