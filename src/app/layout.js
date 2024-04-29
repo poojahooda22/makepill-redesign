c2848003059126444f39ed89633eb45d1b92d45b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-29 11:34:30 +0530

    updated layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        
         {children}
       </body>
     </html>
