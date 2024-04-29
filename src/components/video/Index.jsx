be3e686753b6dda6ebffd20626ab51f0d2afb6d9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-29 17:08:28 +0530

    updated index

--- src/components/video/Index.jsx
+++ src/components/video/Index.jsx
@@ -13,7 +13,6 @@ function Video() {
             className='w-full h-full object-cover'
             src="https://res.cloudinary.com/mkpill/video/upload/v1691655828/makepill_showreel_opti_a5c666148b.mp4"
         >
-
         </video>
       </div>
     </div>
