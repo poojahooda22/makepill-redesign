5d5f86213aa32c710eddf690c7c74c2ced7d98dc
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-29 22:38:21 +0530

    added

--- src/components/header/Index.jsx
+++ src/components/header/Index.jsx
@@ -6,7 +6,7 @@ import { opacity, rotate, rotate2 } from './anime';
 import { useState } from 'react';
 import Nav from './nav/Index';
 import Link from "next/link";
-// import styles fr'
+// import styles '
 
 
 function Header() {
