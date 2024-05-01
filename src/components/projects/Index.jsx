0a824b9a8c49ae477a728fd0642a49be2bfee459
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-01 21:03:44 +0530

    updated index

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -22,7 +22,7 @@ function Projects() {
                 autoPlay
                 loop
                 muted
-                src="https://res.cloudinary.com/mkpill/video/upload/v1689237447/battlehack_reel_vp9_m"
+                src="https://res.cloudinary.com/mkpill/video/upload/v1689237447/battlehack_reel_vp9"
             ><
          */}
         <div className='' >
