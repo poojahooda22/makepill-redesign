1b6309ddf9050169729786d7ed1c3a1a11d987be
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-01 20:46:20 +0530

    updated index

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -24,7 +24,7 @@ function Projects() {
                 muted
                 src="https://res.cloudinary.com/mkpill/video/upload/v1689237447/battlehack_reel_vp9_53ef201263.webm"
             ></video>
-        </ */}
+         */}
         <div className='' >
             <div className='w-full flex items-center justify-center'>
                 {data.map((item, index) => {
