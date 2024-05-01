09eb7ecca9fbc48dda48e66e84da65e0f52f2105
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-01 18:54:17 +0530

    updated index

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 function Projects() {
   return (
     <div>
-      
+        <div>
+            <video 
+                autoPlay
+                loop
+                muted
+                src="https://res.cloudinary.com/mkpill/video/upload/v1689237447/battlehack_reel_vp9_53ef201263.webm"
+            ></video>
+        </div>
+
+        <div>
+            
+        </div>
     </div>
   )
 }
