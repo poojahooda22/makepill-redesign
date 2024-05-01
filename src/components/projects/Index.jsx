66804a5045252f2d9448ea69d72308b4ac08e07a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-01 21:07:34 +0530

    added

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -22,7 +22,7 @@ function Projects() {
                 autoPlay
                 loop
                 muted
-                src="https://res.c
+                src="https://res.
          */}
         <div className='' >
             <div className='w-full flex items-center justify-center'>
