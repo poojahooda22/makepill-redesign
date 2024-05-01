b4e343e2775723d324f2f5e06ff400a58638602f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-01 21:06:38 +0530

    added

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -22,7 +22,7 @@ function Projects() {
                 autoPlay
                 loop
                 muted
-                src="https://res.cloudinary.c
+                src="https://res.cloudinary.
          */}
         <div className='' >
             <div className='w-full flex items-center justify-center'>
