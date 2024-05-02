b85e6060c170473f9424e18d58f65a958e1e480b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-02 22:58:03 +0530

    added

--- src/components/social/Index.jsx
+++ src/components/social/Index.jsx
@@ -59,7 +59,7 @@ function Social() {
                         {/* <div className='SocialContentLayer z-[2] w-full flex absolute top-0 left-0 py-[1.4vw] bg-[#222222] opacity-0'>
                                 <div className='w-full flex gap-[2vw] mr-[2vw] items-center justify-center '>
                                     <h2 className=' sm:text-[1.5vw] tracking-wide block'>{item.title}</h2> 
-                                    <span className='text-[#deff00] '><svg xmlns="http://www.w3.org/2000/svg" className='w-[4vw] sm:w-[1.2vw]' width="23" height="23" fill="none" viewBox="0 0 23 23" data-v-9f3306fa=""><path fill="currentColor" d="M6.629 22.5 3.25 20.091l4.714-6.985L.5 10.456l1.414-3.693L9.38 9.252V.5h4.242v8.752l7.465-2.49 1.414 3.694-7.464 2.65 4.714 6.985-3.379 2.409-4.871-7.387L6.629 22.5Z"></path></svg></span>
+                                    <span className='text-[#deff00] '>s="http://www.w3.org/2000/svg" className='w-[4vw] sm:w-[1.2vw]' width="23" height="23" fill="none" viewBox="0 0 23 23" data-v-9f3306fa=""><path fill="currentColor" d="M6.629 22.5 3.25 20.091l4.714-6.985L.5 10.456l1.414-3.693L9.38 9.252V.5h4.242v8.752l7.465-2.49 1.414 3.694-7.464 2.65 4.714 6.985-3.379 2.409-4.871-7.387L6.629 22.5Z"></path></svg></span>
                                     <h2 className=' sm:text-[1.5vw] text-[#82828.link}</h2>    
                                 </div> ter jus
                                 </div>
