ac5265807a028e0f7435794ca8a534ad9a1eba2c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-02 19:25:56 +0530

    updated

--- src/components/social/Index.jsx
+++ src/components/social/Index.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 function Social() {
+    const data = [
+        {title: 'Instagram',},
+        {title: 'Facebook',},
+        
+    ]
   return (
     <div>
-        <div>
-            <div><h4>/ Follow us</h4></div>
+        <div className='flex items-center justify-center mb-[8vw]'>
+            <div><h4 className='sm:text-[1.2vw]'>/ Follow us</h4></div>
         </div>
+        {data.map((item, index) => {
+            return (
+                <div key={index} className='w-full bg-red-400 flex items-center justify-center'>
+                    <div>
+                        <h2>{item.title}</h2>
+                    </div>
+                </div>
+            )
+        })}
     </div>
   )
 }
