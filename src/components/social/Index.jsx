77ae840adbf48b14cd118083fe8bcacfd0816de9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-02 21:16:03 +0530

    updated index

--- src/components/social/Index.jsx
+++ src/components/social/Index.jsx
@@ -7,6 +7,7 @@ function Social() {
         {title: 'Dribbble', link: '@makepill'},
         {title: 'YouTube', link: '@makepill'},
         {title: 'LinkedIn', link: '@makepill'},
+        
     ]
 
 
@@ -21,7 +22,14 @@ function Social() {
                     <div key={index} className='w-full  flex items-center justify-center relative hover:cursor-pointer'>
                         <div className='w-full py-[1.4vw] flex items-center justify-center'>
                             <h2 className='sm:text-[1.5vw] tracking-wide text-[#828282] font-semibold'>{item.title}</h2>   
-                        </div>   
+                        </div> 
+                        <div className='w-full absolute top-0 left-0 bg-green-400 py-[1.4vw]'>
+                            <div className='flex'>
+                                <h2>{item.title}</h2>
+                                <span><svg xmlns="http://www.w3.org/2000/svg" className='w-[4vw] sm:w-[2vw]' width="23" height="23" fill="none" viewBox="0 0 23 23" data-v-9f3306fa=""><path fill="currentColor" d="M6.629 22.5 3.25 20.091l4.714-6.985L.5 10.456l1.414-3.693L9.38 9.252V.5h4.242v8.752l7.465-2.49 1.414 3.694-7.464 2.65 4.714 6.985-3.379 2.409-4.871-7.387L6.629 22.5Z"></path></svg></span>
+                                <h2>{item.link}</h2>    
+                            </div>    
+                        </div>  
                     </div>
                 )
             })}
