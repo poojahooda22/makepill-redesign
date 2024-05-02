c7921f6fcc0798676bf7f2bbcb3c8c69a0ffd802
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-02 18:44:40 +0530

    created index

---
+++ src/components/social/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Social() {
+  return (
+    <div>
+        <div>
+            <div><h4>/ Follow us</h4></div>
+        </div>
+    </div>
+  )
+}
+
+export default Social
