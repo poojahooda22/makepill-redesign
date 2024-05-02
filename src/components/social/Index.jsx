c57af12cbafe67f088761235e04fd3ae8e779df4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-02 19:32:02 +0530

    updated index

--- src/components/social/Index.jsx
+++ src/components/social/Index.jsx
@@ -4,7 +4,7 @@ function Social() {
     const data = [
         {title: 'Instagram',},
         {title: 'Facebook',},
-        
+
     ]
   return (
     <div>
@@ -13,9 +13,9 @@ function Social() {
         </div>
         {data.map((item, index) => {
             return (
-                <div key={index} className='w-full bg-red-400 flex items-center justify-center'>
-                    <div>
-                        <h2>{item.title}</h2>
+                <div key={index} className='w-full  flex items-center justify-center'>
+                    <div className='w-full py-[1.4vw] flex items-center justify-center'>
+                        <h2 className='sm:text-[1.5vw] tracking-wide text-[#828282] font-medium'>{item.title}</h2>
                     </div>
                 </div>
             )
