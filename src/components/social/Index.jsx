33f7374f87c92eec613bb43552eedd7f5b73615c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-03 14:48:02 +0530

    updated index

--- src/components/social/Index.jsx
+++ src/components/social/Index.jsx
@@ -23,14 +23,14 @@ function Social() {
             {data.map((item, index) => {
                 return (
                     <div key={index} className='socialDiv w-full  flex items-center justify-center relative hover:cursor-pointer]'>
-                        <div className='SocialContentLayer z-[2] absolute bottom-0 left-0 bg-[#222222] py-[1.4vw] overflow-hidden opacity-1'>
-                               <div className={`bgcnt ${style.bgcnt} w-full flex h-[100%] items-center justify-center`}>
+                        <div className='SocialContentLayer z-[2] w-full h-[100%] absolute bottom-0 left-0 border-[1px] py-[1.4vw] overflow-hidden opacity-1'>
+                               {/* <div className={`bgcnt ${style.bgcnt} w-full flex h-[100%] items-center justify-center`}>
                                     <div className={`elem ${style.elem} w-full flex gap-[2vw] mr-[2vw] items-center justify-center `}>
                                         <h2 className=' sm:text-[1.5vw] tracking-wide block'>{item.title}</h2> 
                                         <span className='text-[#deff00] '><svg xmlns="http://www.w3.org/2000/svg" className='w-[4vw] sm:w-[1.2vw]' width="23" height="23" fill="none" viewBox="0 0 23 23" data-v-9f3306fa=""><path fill="currentColor" d="M6.629 22.5 3.25 20.091l4.714-6.985L.5 10.456l1.414-3.693L9.38 9.252V.5h4.242v8.752l7.465-2.49 1.414 3.694-7.464 2.65 4.714 6.985-3.379 2.409-4.871-7.387L6.629 22.5Z"></path></svg></span>
                                         <h2 className=' sm:text-[1.5vw] text-[#828282] inline-block '>{item.link}</h2>    
                                     </div>  
-                                </div>   
+                                </div>    */}
                         </div> 
                         <div className='socialh2 w-full py-[1.4vw] z-[1] flex items-center justify-center overflow-hidden relative  '>
                             <h2 className=' sm:text-[1.7vw] tracking-wide text-[#828282] font-semibold'>{item.title}</h2>                        
