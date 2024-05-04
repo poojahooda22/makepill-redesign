22a20db836a7a7c9ab0ac186fd5501655cdd1535
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-04 21:39:56 +0530

    added

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -19,7 +19,7 @@ function Projects() {
     //             s .2,
     //             ease:r3            
     //         })
-    //         gsap.to('.pro', {
+    //         gsap.to', {
     //             opacity: 0,
     //             stagger: .2,
     //             ease: Power3            
