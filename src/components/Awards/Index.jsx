2f9b327e1cc5e9f5baf3438383b4bd7b4d15017c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-04 17:54:55 +0530

    updated index

--- src/components/Awards/Index.jsx
+++ src/components/Awards/Index.jsx
@@ -35,7 +35,7 @@ function AwardsSection() {
     >
         <div className='py-[6vw] sm:py-0 px-[5vw] sm:px-[13vw]  sm:flex items-start justify-between sm:mb-[4vw] '>
             <div className='leftAwardDiv  '>
-                <div className=' sm:space-y-[4vw] pb-[10vw] sm:pb-0'>
+                <div className=' sm:space-y-[4vw] pb-[14vw] sm:pb-0'>
                     <h4 className='text-[5vw] sm:text-[1.1vw] tracking-wider'>/ Awards & Recognitions</h4>
                 </div>  
             </div>
@@ -43,19 +43,19 @@ function AwardsSection() {
                 <div>
                     {data.map((item, index) => {
                         return (
-                            <div key={index} className='sm:mb-[2vw] mb-[8vw]'>
-                                <div className='flex sm:items-end sm:justify-between sm:mb-[2vw] mb-[8vw] leading-none'>
-                                    <h4 className='text-[4.1vw] sm:text-[1.4vw]'>{item.title}</h4>
-                                    <p className='text-[4.1vw] sm:text-[1.2vw] text-[#deff00] '>{item.count}</p>
+                            <div key={index} className='sm:mb-[2vw] mb-[12vw]'>
+                                <div className='flex sm:items-end sm:justify-between sm:mb-[2vw] mb-[12vw] leading-none'>
+                                    <h4 className='text-[4.5vw] sm:text-[1.4vw]'>{item.title}</h4>
+                                    <p className='text-[4.5vw] sm:text-[1.2vw] text-[#deff00] '>{item.count}</p>
                                 </div>
                                 <div className='flex items-end justify-between leading-none'>
-                                    <h6 className='text-[4.1vw] sm:text-[1.2vw] '>{item.award}</h6>
-                                    <p className='text-[4.1vw] sm:text-[1.2vw] '>{item.date}</p>
+                                    <h6 className='text-[4.2vw] sm:text-[1.2vw] '>{item.award}</h6>
+                                    <p className='text-[4.2vw] sm:text-[1.2vw] '>{item.date}</p>
                                 </div>
                                 {item.award1 && (
-                                    <div className='flex  items-end  justify-between mb-[8vw] sm:mb-[2.5vw] space-y-[4vw] sm:space-y-[1.2vw]'>
-                                        <h6 className='text-[4.1vw] sm:text-[1.2vw]'>{item.award1}</h6>
-                                        <p className='text-[4.1vw] sm:text-[1.2vw]'>{item.date1}</p>
+                                    <div className='flex  items-end  justify-between mb-[12vw] sm:mb-[2.5vw] space-y-[6vw] sm:space-y-[1.2vw]'>
+                                        <h6 className='text-[4.2vw] sm:text-[1.2vw]'>{item.award1}</h6>
+                                        <p className='text-[4.2vw] sm:text-[1.2vw]'>{item.date1}</p>
                                     </div>
                                 )}
                             </div>
