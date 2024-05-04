79b59594af512dccb266487ca3fc3f6b6e02ea0f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-04 17:55:08 +0530

    created index

---
+++ src/components/footer/Index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full relative'>
+      <div className='w-full flex items-center justify-center'>
+        <div>
+          <video autoPlay loop muted src="https://res.cloudinary.com/mkpill/video/upload/v1689376834/footer_x1_vp9_4d2ebedd3d.webm"></video>
+        </div>
+        <div>
+          <h1>Brief us now</h1>
+          <h2>Let&apos;s build now together your tomorrow&apos;s project</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
