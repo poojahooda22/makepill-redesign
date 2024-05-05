71f5cbfa76221c1c6bd7c548ada0ad865a846d4f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-05 19:51:26 +0530

    index

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -9,7 +9,7 @@ function Projects() {
     //     divProject.addEventLouseenter', () => {
     //         gsap.froojeight > div', {
     //             
-    //             opac                      
+    //                       
     //     })
     // })
 
