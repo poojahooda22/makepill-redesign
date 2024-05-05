4750ef5f6511f12619914425fa3cf28cfc6357a4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-05 20:01:15 +0530

    added

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -5,7 +5,7 @@ import { Power3 } from 'gsap';
 
 function Projects() {
     // useEffect(() => {
-    //     const divProject = documeqeor(r');                            
+    //     const divProjectqeor(r');                            
     // })
 
     const data = [
