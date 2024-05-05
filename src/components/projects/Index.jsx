66f0fa3d822e531e8f440564157b431d20e79f60
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-05 19:56:33 +0530

    updated

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -6,7 +6,7 @@ import { Power3 } from 'gsap';
 function Projects() {
     // useEffect(() => {
     //     const divProject = document.querySelector('.projectheadDiv');
-    //     divProject.addEn) => {
+    //     divProject.add) => {
     //         gsap.f                          
     // })
 
