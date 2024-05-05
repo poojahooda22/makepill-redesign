8edbfe3a0fd1e0ba463042f461bfaeb754b1ee39
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-05 17:45:22 +0530

    updated index

--- src/components/services/Index.jsx
+++ src/components/services/Index.jsx
@@ -1,6 +1,8 @@
 import gsap from 'gsap'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from './style.mosule.css'
+import { useGSAP } from '@gsap/react'
+import { opacity } from '../header/anime';
 // const white = {background: 'white', color: '#0f0f0f'}
 // const black = {background: '#0f0f0f', color: 'white'}
 
@@ -31,9 +33,37 @@ function About() {
     
   // })
 
+  const serviceRef = useRef();
+  const serviceRef2 = useRef();
+  const serviceRef3 = useRef();
+
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.aboutSec .rightDiv',
+        start: 'top 90%',
+        end: 'top 30%',
+        scrub:3,
+        markers: true,
+      }, 
+    });
+    tl.from(serviceRef2.current, {
+      x: -50,
+      opacity: 0,
+      duration: 1,
+      ease: 'power4.in'
+    })
+    tl.from('.elemtxt h1', {
+      y: 100,
+      opacity: 0,
+      stagger: 0.4,
+      duration: 1,
+    })
+  })
+
   
   return (
-    <div
+    <div ref= {serviceRef}
       data-scroll-section data-scroll-speed=".1" 
       className='aboutSec section w-full pt-[6vw] overflow-hidden '
     >
@@ -51,30 +81,14 @@ function About() {
         </div>
         <div className='rightDiv sm:w-1/2 px-[5vw] sm:px-0'>
           <div className=' sm:space-y-[4vw] space-y-[12vw]'>
-            <h4 className='text-[5vw] sm:text-[1vw] tracking-wider'>/ Give life to your ideas</h4>
-            <div className=' sm:w-3/4 text-[6vw] sm:text-[2.3vw] font-medium tracking-wide sm:leading-[3vw]'>
-              <div>
-                <h1>We pool our expertise to turn your</h1>
-              </div>
-              <div>
-                <h1>wildest projects into singular</h1>
-              </div>
-              <div className='mb-[12vw] sm:mb-[4vw]'>
-                <h1>
-                  experiences.
-                </h1>
-              </div>
-              <div>
-                <h1>
-                  Discover our diverse expertise,
-                </h1>
-              </div>
-              <div>
-                <h1>
-                  each one providing a unique
-                </h1>
-              </div>
-              <div><h1>dimension to your vision.</h1></div>  
+            <div className='overflow-hidden'><h4 ref={serviceRef2} className='firsth4 text-[5vw] sm:text-[1vw] tracking-wider'>/ Give life to your ideas</h4></div>
+            <div className='serviceh1 sm:w-3/4 text-[6vw] sm:text-[2.3vw] font-medium tracking-wide sm:leading-[3vw]'>
+              <div className='elemtxt overflow-hidden'><h1>We pool our expertise to turn your</h1></div>
+              <div className='elemtxt overflow-hidden'><h1>wildest projects into singular</h1></div>
+              <div className='elemtxt mb-[12vw] sm:mb-[4vw] overflow-hidden'><h1>experiences.</h1></div>
+              <div className='elemtxt overflow-hidden'><h1>Discover our diverse expertise,</h1></div>
+              <div className='elemtxt overflow-hidden'><h1>each one providing a unique</h1></div>
+              <div className='elemtxt overflow-hidden'><h1>dimension to your vision.</h1></div>  
             </div>
             <div className='flex items-center justify-center sm:items-start sm:justify-start '>
               <div className='serviceBtn w-[28vw] h-[28vw] sm:w-[8vw] sm:h-[8vw] rounded-full border-[.8px] border-[#d9d9d9] flex items-center justify-center'>
