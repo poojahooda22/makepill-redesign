e7bc1d96f4b2f57a3b75b3f34ff0305e053ead42
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-05 19:54:51 +0530

    updated index

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -7,7 +7,7 @@ function Projects() {
     // useEffect(() => {
     //     const divProject = document.querySelector('.projectheadDiv');
     //     divProject.addEventLouseenter', () => {
-    //         gsap.froo>                          
+    //         gsap.fro>                          
     // })
 
     const data = [
