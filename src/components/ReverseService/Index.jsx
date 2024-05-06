5064b55272fe4dbc1d8713079386d7e725074ecb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-06 14:28:15 +0530

    updated index

--- src/components/ReverseService/Index.jsx
+++ src/components/ReverseService/Index.jsx
@@ -1,8 +1,9 @@
 import gsap from 'gsap'
-import React, { useEffect } from 'react'
+import React, { useEffect,  useRef } from 'react'
 import styles from './Style.module.css'
 // const white = {background: 'white', color: '#0f0f0f'}
 // const black = {background: '#0f0f0f', color: 'white'}
+import { useGSAP } from '@gsap/react'
 
 function ReverseService() {
 
@@ -31,6 +32,46 @@ function ReverseService() {
     
 //   })
 
+const serviceRef = useRef();
+const serviceRef2 = useRef();
+
+useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.serviceSec .reverseLeftDiv',
+        start: 'top 90%',
+        end: 'top 30%',
+        scrub:3,
+      }, 
+    });
+    
+    tl.from(serviceRef2.current, {
+      x: -50,
+      opacity: 0,
+      duration: 1,
+      ease: 'power4.in'
+    })
+    
+    tl.from('.serviceReversetxt h1', {
+      y: 100,
+      opacity: 0,
+      stagger: 0.4,
+      duration: 1,
+    }, 'a')
+    tl.from('.rightDivvideo video', {
+      x: 100,
+      opacity: 0,
+        duration: 1,
+    }, 'a')
+    tl.from('.reverseServicep', {
+        x: -50,
+      opacity: 0,
+      duration: 1,
+      ease: 'power4.in'
+    })
+    
+  })
+
   
   return (
     <div
@@ -40,31 +81,31 @@ function ReverseService() {
         <div className='py-[6vw] sm:py-0  sm:flex items-center justify-between px-[5vw] sm:px-[2.5vw] sm:mb-[4vw]'>
             <div className='reverseLeftDiv w-full sm:w-2/3  sm:px-0 mb-[5vw] sm:mb-0'>
                 <div className=' sm:space-y-[4vw] space-y-[12vw] pl-0 sm:pl-[10vw] px-[5vw] '>
-                    <h4 className='text-[5vw] sm:text-[1.1vw] tracking-wider'>/ Our culture</h4>
-                    <div className=' text-[6vw] sm:text-[2.3vw] font-medium tracking-wide sm:leading-[3vw]'>
-                        <div>
+                    <div ref={serviceRef2} className='overflow-hidden'><h4   className='text-[5vw] sm:text-[1.1vw] tracking-wider'>/ Our culture</h4></div>
+                    <div className='serviceReversetxt text-[6vw] sm:text-[2.3vw] font-medium tracking-wide sm:leading-[3vw]'>
+                        <div className='overflow-hidden'>
                             <h1>Human values and high standards</h1>
                         </div>
-                        <div>
+                        <div className='overflow-hidden'>
                             <h1>are core components of our teams&apos;s</h1>
                         </div>
-                        <div className='mb-[12vw] sm:mb-[4vw]'>
+                        <div className='mb-[12vw] sm:mb-[4vw] overflow-hidden'>
                             <h1>
                             philosophy and culture
                             </h1>
                         </div>
-                        <div>
+                        <div className='overflow-hidden'>
                             <h1>
                             Whether PM/PO*, designers or
                             </h1>
                         </div>
-                        <div>
+                        <div className='overflow-hidden'>
                             <h1>
                                 developers, we all strive for
                             </h1>
                         </div>
-                        <div><h1>excellence in designing ambitious,</h1></div> 
-                        <div><h1>innovative projects.</h1></div> 
+                        <div className='overflow-hidden'><h1>excellence in designing ambitious,</h1></div> 
+                        <div className='overflow-hidden'><h1>innovative projects.</h1></div> 
                     </div>
                     <div className='flex items-center justify-center sm:items-start sm:justify-start '>
                         <div className='serviceBtn w-[28vw] h-[28vw] sm:w-[8vw] sm:h-[8vw] rounded-full border-[.8px] border-[#d9d9d9] flex items-center justify-center'>
@@ -74,7 +115,7 @@ function ReverseService() {
                 </div>  
             </div>
             <div className='reverseRightDiv  sm:w-1/2 flex items-center justify-center  mb-[16vw] sm:mb-0 '>
-                <div className='leftvideo h-[70vw] sm:h-[30vw] mt-[10vw] sm:mt-0'>
+                <div className='rightDivvideo h-[70vw] sm:h-[30vw] mt-[10vw] sm:mt-0'>
                     <video 
                     autoPlay
                     loop
@@ -84,7 +125,7 @@ function ReverseService() {
                 </div>
             </div>
         </div> 
-        <div ><p className='mt-[10vw] px-[5vw] sm:pl-[12.5vw] text-[5vw] sm:text-[1.1vw] tracking-wider'>/ Our clients</p></div>
+        <div className='overflow-hidden' ><p className='reverseServicep mt-[10vw] px-[5vw] sm:pl-[12.5vw] text-[5vw] sm:text-[1.1vw] tracking-wider'>/ Our clients</p></div>
         <div className='my-[24vw] sm:my-[4vw] space-y-[1vw]'>    
             <div className={`elem ${styles.elem} flex items-center`}>
                 {['Seela', 'Airbus', 'Sportfunding', 'LVMH', 'Flexyroom', 'Push&Sell', 'Seela', 'Airbus', 'Sportfunding', 'LVMH', 'Flexyroom', 'Push&Sell'].map((item, index) => {
