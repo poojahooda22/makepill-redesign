b3db4007f465fc0a5290c06b7cf22cf5ee0a20e9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-07 20:52:34 +0530

    added

--- src/components/ReverseService/Index.jsx
+++ src/components/ReverseService/Index.jsx
@@ -9,7 +9,7 @@ import { useGSAP } from '@gsap/react'
 
 function ReverseService() {
 
-//   // const [color, setColor] = React.useState()
+//   // const [color, setColor] = React.useStat
 
 //     useEffect(() => {
 //     const section = document.querySelector('.serviceSec')
