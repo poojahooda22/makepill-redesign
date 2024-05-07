f9d83ec14822bfa3853b9eaa42f7490fca973784
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-07 21:23:52 +0530

    created page

---
+++ src/app/aboutPage/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div className='text-white font-[mainfont] bg-primary-main '>
+        <h1>Coming Soon</h1>
+    </div>
+  )
+}
+
+export default AboutPage
