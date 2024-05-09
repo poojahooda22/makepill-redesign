cc119ed8260f2ec491b01c0204ca45b57edd72e1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-09 21:17:17 +0530

    updated cursor

--- src/components/cursor/Index.jsx
+++ src/components/cursor/Index.jsx
@@ -1,85 +1,109 @@
 'use client'
 
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
+// import styles from './style.module.css'
+// // import gsap from 'gsap'
+
+// function Cursor() {
+    
+//   useEffect(() => {
+
+//     const circleElement = document.querySelector('.cursor');
+
+//     // Create objects to track mouse position and custom cursor position
+//     const mouse = { x: 0, y: 0 }; // Track current mouse position
+//     const previousMouse = { x: 0, y: 0 } // Store the previous mouse position
+//     const circle = { x: 0, y: 0 }; // Track the circle position
+
+//     // Initialize variables to track scaling and rotation
+//     let currentScale = 0; // Track current scale value
+//     let currentAngle = 0; // Track current angle value
+
+//     // Update mouse position on the 'mousemove' event
+//     global.window.addEventListener('mousemove', (e) => {
+//       mouse.x = e.x ;
+//       mouse.y = e.y ;
+//     });
+
+//     // Smoothing factor for cursor movement speed (0 = smoother, 1 = instant)
+//     const speed = 0.15;
+
+//     // Start animation
+//     const tick = () => {
+//       // MOVE
+//       // Calculate circle movement based on mouse position and smoothing
+//       circle.x += (mouse.x - circle.x) * speed;
+//       circle.y += (mouse.y - circle.y) * speed;
+//       // Create a transformation string for cursor translation
+//       const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
+
+//       // SQUEEZE
+//       // 1. Calculate the change in mouse position (deltaMouse)
+//       const deltaMouseX = mouse.x - previousMouse.x;
+//       const deltaMouseY = mouse.y - previousMouse.y;
+//       // Update previous mouse position for the next frame
+//       previousMouse.x = mouse.x;
+//       previousMouse.y = mouse.y;
+//       // 2. Calculate mouse velocity using Pythagorean theorem and adjust speed
+//       const mouseVelocity = Math.min(Math.sqrt(deltaMouseX**2 + deltaMouseY**2) * 4, 150); 
+//       // 3. Convert mouse velocity to a value in the range [0, 0.5]
+//       const scaleValue = (mouseVelocity / 150) * 0.5;
+//       // 4. Smoothly update the current scale
+//       currentScale += (scaleValue - currentScale) * speed;
+//       // 5. Create a transformation string for scaling
+//       const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
+
+//       // ROTATE
+//       // 1. Calculate the angle using the atan2 function
+//       const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
+//       // 2. Check for a threshold to reduce shakiness at low mouse velocity
+//       if (mouseVelocity > 20) {
+//         currentAngle = angle;
+//       }
+//       // 3. Create a transformation string for rotation
+//       const rotateTransform = `rotate(${currentAngle}deg)`;
+
+//       // Apply all transformations to the circle element in a specific order: translate -> rotate -> scale
+//       circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
+
+//       // Request the next frame to continue the animation
+//       window.requestAnimationFrame(tick);
+//     }
+//     tick();
+//   })
+
+//   return (
+//     <div className=''>
+//       <div>
+//         <div className={`cursor ${styles.cursor} hidden sm:flex items-center justify-center relative`}>
+//           {/* <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[1vw]'><h4>See project</h4></div> */}
+//         </div>
+//       </div>
+         
+//     </div>
+//   )
+// }
+
+// export default Cursor
+
+
+
+import React, { useEffect } from 'react'
 import styles from './style.module.css'
-// import gsap from 'gsap'
+
 
 function Cursor() {
-    
+
+  const [mousePosition, setmousePosition] = React.useState({ x: 0, y: 0 });
+
   useEffect(() => {
 
-    const circleElement = document.querySelector('.cursor');
 
-    // Create objects to track mouse position and custom cursor position
-    const mouse = { x: 0, y: 0 }; // Track current mouse position
-    const previousMouse = { x: 0, y: 0 } // Store the previous mouse position
-    const circle = { x: 0, y: 0 }; // Track the circle position
-
-    // Initialize variables to track scaling and rotation
-    let currentScale = 0; // Track current scale value
-    let currentAngle = 0; // Track current angle value
-
-    // Update mouse position on the 'mousemove' event
-    global.window.addEventListener('mousemove', (e) => {
-      mouse.x = e.x ;
-      mouse.y = e.y ;
-    });
-
-    // Smoothing factor for cursor movement speed (0 = smoother, 1 = instant)
-    const speed = 0.15;
-
-    // Start animation
-    const tick = () => {
-      // MOVE
-      // Calculate circle movement based on mouse position and smoothing
-      circle.x += (mouse.x - circle.x) * speed;
-      circle.y += (mouse.y - circle.y) * speed;
-      // Create a transformation string for cursor translation
-      const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
-
-      // SQUEEZE
-      // 1. Calculate the change in mouse position (deltaMouse)
-      const deltaMouseX = mouse.x - previousMouse.x;
-      const deltaMouseY = mouse.y - previousMouse.y;
-      // Update previous mouse position for the next frame
-      previousMouse.x = mouse.x;
-      previousMouse.y = mouse.y;
-      // 2. Calculate mouse velocity using Pythagorean theorem and adjust speed
-      const mouseVelocity = Math.min(Math.sqrt(deltaMouseX**2 + deltaMouseY**2) * 4, 150); 
-      // 3. Convert mouse velocity to a value in the range [0, 0.5]
-      const scaleValue = (mouseVelocity / 150) * 0.5;
-      // 4. Smoothly update the current scale
-      currentScale += (scaleValue - currentScale) * speed;
-      // 5. Create a transformation string for scaling
-      const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
-
-      // ROTATE
-      // 1. Calculate the angle using the atan2 function
-      const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
-      // 2. Check for a threshold to reduce shakiness at low mouse velocity
-      if (mouseVelocity > 20) {
-        currentAngle = angle;
-      }
-      // 3. Create a transformation string for rotation
-      const rotateTransform = `rotate(${currentAngle}deg)`;
-
-      // Apply all transformations to the circle element in a specific order: translate -> rotate -> scale
-      circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
-
-      // Request the next frame to continue the animation
-      global.window.requestAnimationFrame(tick);
-    }
-    tick();
-  })
+  }, [])
 
   return (
-    <div className=''>
-      <div>
-        <div className={`cursor ${styles.cursor} hidden sm:flex items-center justify-center relative`}>
-          {/* <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[1vw]'><h4>See project</h4></div> */}
-        </div>
-      </div>
-         
+    <div>
+      <div className={`cursor ${styles.cursor} hidden sm:flex items-center justify-center relative`}></div>
     </div>
   )
 }
