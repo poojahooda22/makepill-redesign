63bedaea266c10f420c36be5b0cb3015b656fa3e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-09 22:03:28 +0530

    added

--- src/components/services/Index.jsx
+++ src/components/services/Index.jsx
@@ -6,7 +6,7 @@ import styles from './style.mosule.css'
 import { useGSAP } from '@gsap/react'
 import { opacity } from '../header/anime';
 // const white = {backgroun: 'white', color: 'f0f0f'}
-// const blck = {backgro: #f, colr: 'whe'}
+// const blck = {backgro: #f colr: 'whe'}
 
 function About() {
 
