6069f1d78d433bb8ea0393014590d88dc06447a9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-10 22:24:18 +0530

    added

--- src/components/cursor/Index.jsx
+++ src/components/cursor/Index.jsx
@@ -47,7 +47,7 @@ function Cursor() {
         previousMouse.y = mouse.y;
         // 2. Calculate mouse velocity using Pythagorean theorem and adjust speed
         const mouseVelocity = Math.min(Math.sqrt(deltaMouseX**2 + deltaMouseY**2) * 4, 150); 
-      // 3. Convert mouse velocity to a value in the range [0, 0.5]
+        // 3. Convert mouse velocity to a value in the range [0, 0.5]
       const scaleValue = (mouseVelocity / 150) * 0.5;
       // 4. Smoothly update the current scale
       currentScale += (scaleValue - currentScale) * speed;
