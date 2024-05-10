29b597187e19ef1b0fd6674e389ecb301e041e4b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-10 22:25:27 +0530

    added

--- src/components/cursor/Index.jsx
+++ src/components/cursor/Index.jsx
@@ -56,7 +56,7 @@ function Cursor() {
 
         // ROTATE
         // 1. Calculate the angle using the atan2 function
-      const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
+        const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
       // 2. Check for a threshold to reduce shakiness at low mouse velocity
       if (mouseVelocity > 20) {
         currentAngle = angle;
