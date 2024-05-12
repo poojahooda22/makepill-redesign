05df450438ca0a931bdeb355194a7c7924212259
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-12 18:24:56 +0530

    updated index

--- src/components/video/Index.jsx
+++ src/components/video/Index.jsx
@@ -30,7 +30,7 @@ function Video() {
   }
   
   return (
-    <div  data-scroll data-scroll-section  data-scroll-speed="0."   className=' videoDiv w-full'>
+    <div  data-scroll data-scroll-section  data-scroll-speed="0.1"   className=' videoDiv w-full'>
       <div className=' videoDiv h-[60vh] sm:h-full '
         onMouseEnter={handleCursor}
         onMouseLeave={handleCursorLeave}
