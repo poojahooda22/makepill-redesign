923bf5afb4a774ad65243b57b882950dc0ca50de
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-14 18:21:55 +0530

    updated about

--- src/app/aboutPage/page.jsx
+++ src/app/aboutPage/page.jsx
@@ -1,9 +1,11 @@
+import Header from '@/components/header/Index'
 import React from 'react'
 
 function AboutPage() {
   return (
     <div className='w-full h-[100vh] text-white font-[mainfont] bg-primary-main flex items-center justify-center '>
-        <div className='sm:text-[4vw]'><h1>Coming Soon...</h1></div>
+      <Header/>
+      <div className='sm:text-[4vw]'><h1>Coming Soon...</h1></div>
     </div>
   )
 }
