0764a0102bff3d24a27fb9f9a3a188bae119d3cc
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-14 11:47:57 +0530

    updated index

--- src/components/video/Index.jsx
+++ src/components/video/Index.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useRef, useState } from 'react'
-import  gsap  from 'gsap'
+import  {gsap } from 'gsap'
 import { IoVolumeMediumSharp } from "react-icons/io5";
 import { MdVolumeUp } from "react-icons/md";
 import { useGSAP } from '@gsap/react';
@@ -9,20 +9,50 @@ import MouseFollower from "mouse-follower";
 
 
 function Video() {
-  const cursor = new MouseFollower();
+  // const cursor = new MouseFollower();
   
   // const [cursorChange, setCursorChange] = useState(false);
 
   // const handleCursorLeave = () => {
   // 
   // }
+
+
+  useEffect(() => {
+    const cursor = document.querySelector('.cursor');
+
+    const videoDiv = document.querySelector('.videoDiv');
+
+    videoDiv.addEventListener('mouseenter', function () {
+      console.log('mouse enter');
+      cursor.innerHTML = "View More"
+      gsap.to(cursor, {
+        width: '160px',
+        height: '160px',
+        duration: 0.5,
+      })
+    })
+    videoDiv.addEventListener('mouseleave', function () {
+      console.log('mouse enter');
+      cursor.innerHTML = ""
+      gsap.to(cursor, {
+        width: '16px',
+        height: '16px',
+        duration: 0.5,
+      })
+    })
+
+  
+  })
   
   return (
-    <div  data-scroll data-scroll-section data-cursor-icon="See Video"  className=' videoDiv w-full'>
-      <div className=' videoDiv h-[60vh] sm:h-full '
+    <div  data-scroll data-scroll-section  className=' videoDiv w-full'>
+      <div className='w-full h-[60vh] sm:h-full '
         // onMouseEnter={handleCursor}
         // onMouseLeave={handleCursorLeave}
       >
+        <div className='overlay bg-transparent
+         h-[100%] w-[100%] absolute z-[10]'></div>
         <video
           autoPlay
           loop
