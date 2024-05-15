4773158efaa87c8f3f2488898ea6d346950091dc
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-15 21:26:13 +0530

    added

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -149,7 +149,7 @@ function Projects() {
 
     //     clip.forEach(elem => {
     //         elem.addEventListener('mouseenter', () => {
-    //             const targetImageId = 
+    //             const target
 
     //             currentImageIdd = targetImageId;
     //         })
