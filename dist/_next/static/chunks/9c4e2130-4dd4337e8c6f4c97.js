2f4a0e9958afd52f1553a2fe0d1faff282e3d52f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 23:04:51 +0530

    created

---
+++ dist/_next/static/chunks/9c4e2130-4dd4337e8c6f4c97.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[706],{5097:function(t,n,e){e.d(n,{IOM:function(){return i}});var r=e(1810);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M368 368 144 144m224 0L144 368"},child:[]}]})(t)}}}]);
